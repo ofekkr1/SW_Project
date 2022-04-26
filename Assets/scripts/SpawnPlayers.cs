645459a3ca143757c5f907b590cded890dd7f47a
ofekkr1 <ofekkr@gmail.com>
2022-04-26 10:54:02 +0300

    ....

--- Assets/scripts/SpawnPlayers.cs
+++ Assets/scripts/SpawnPlayers.cs
@@ -7,26 +7,21 @@ using UnityEngine.SceneManagement;
 public class SpawnPlayers : MonoBehaviour
 {
     public GameObject playerprefab;
-<<<<<<< Updated upstream
+
     public float minX;
     public float minY;
     public float maxX;
     public float maxY;
-=======
-   // public float minX;
-    //public float minY;
-    //public float maxX;
-    //public float maxY;
->>>>>>> Stashed changes
+
 
     private void Start()
     {
         
-<<<<<<< Updated upstream
-        Vector2 randomPosition = new Vector2(Random.Range(1,1), Random.Range(1,1));
-=======
+
+     
+
         Vector2 randomPosition = new Vector2(1, 1);
->>>>>>> Stashed changes
+
         PhotonNetwork.Instantiate(playerprefab.name, randomPosition, Quaternion.identity);
     }
 }
